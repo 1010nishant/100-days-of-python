8735ee8d3a9f6300a83f7924b98a9fc8c7159465
Nishant-Jangid <88904952+1010nishant@users.noreply.github.com>
2023-01-11 18:40:32 +0000

    03 comment and escape sequence

---
+++ day03-comment_print/index.py
@@ -0,0 +1,20 @@
+print("single line comment")
+
+# this is single line comment
+
+print("this is multi line comment")
+
+'''
+this 
+is multi
+line 
+comment
+'''
+
+# escape sequence
+# print("This doesnt "execute")
+print("This will \" execute")
+# \n will end the line and start at new line of following text
+print("this is boy \nthis is girl")
+
+print("hello","world",1,1, sep="~")
